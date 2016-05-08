5bc908345085f369943575ca73ddbbcf41585710
Yim <yiyan.lu@me.com>
2016-05-08 12:59:29 +0800

    fix<etcd/heartbeat>: fixed hearbeat error interval time

--- meta/heartbeat.go
+++ meta/heartbeat.go
@@ -1,20 +1,28 @@
 package meta
 
-import "time"
+import (
+	"time"
+
+	"github.com/golang/glog"
+)
 
 // CreateAndHeartBeat create a k/v in etcd. And half a ttl, is will refresh the kv's ttl. If fail, will return
 // an error. If error is nil it will return a named Stop function, it will stop the update tick.
 func (m *Info) CreateAndHeartBeat(key, value string, ttl uint64, isThrow bool) (stop func(), err error) {
+	timeout := time.Duration(ttl) * time.Second
 	_, err = m.Create(key, value, ttl)
 	if err != nil {
 		return nil, err
 	}
-	tick := time.NewTicker(time.Duration(ttl / 2))
+	inter := timeout / 2
+	glog.Infof("CreateAndHeartBeat interval(%v)", inter)
+	tick := time.NewTicker(inter)
 	go func() {
 		for {
 			_, ok := <-tick.C
 			if !ok {
 				// stop
+				glog.Info("tick is stoped")
 				return
 			}
 			// update
