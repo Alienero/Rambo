59d74e56022f8d621b4d13840a890dc1749fda32
Yim <yiyan.lu@me.com>
2016-05-13 15:41:16 +0800

    fix<result>: remove result sort test

--- server/ddl.sql
+++ server/ddl.sql
@@ -1,1 +1,7 @@
-Create table test5 (id int PRIMARY KEY AUTO_INCREMENT,b int not null,UNIQUE(b),info varchar(100),c int,  cc bigint(64) unsigned );
+--  id | b | info |
+-- Create table t1 (
+--         id int PRIMARY KEY AUTO_INCREMENT,
+--         b int not null,UNIQUE(b),
+--         info varchar(100)
+-- );
+Create table t1 (id int PRIMARY KEY AUTO_INCREMENT,b int not null,UNIQUE(b),info varchar(100));
